1a7e2311487e214751096c023e11f3c700c8b31d
Reese Pollard <rpollard@gmail.com>
2022-08-14 10:34:37 -0700

    5.17 test for login page on load

--- cypress/e2e/blog_app.cy.js
+++ cypress/e2e/blog_app.cy.js
@@ -1,5 +1,10 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('Blog app', function() {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
+  })
+
+  it('displays the login form', function() {
+    cy.contains('log in to application')
   })
 })
